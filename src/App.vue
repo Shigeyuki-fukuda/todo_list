b4b11caa005cab68dd7089050d1b1a8487346efc
Shigeyuki-fukuda <fukuda@stores.jp>
2018-06-27 23:36:38 +0900

    Bulmaを導入するところまで

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <section class="section">
+   <AddForm></AddForm>
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddForm from './components/AddForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AddForm
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass"; /* 追加 */
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 </style>
---
+++ src/components/AddForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="field">
+     <div class="control">
+      <input class="input is-primary" type="text" placeholder="Primary input">
+     </div>
+    </div>
+    <a class="button is-primary">Primary</a>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import "../../node_modules/bulma/bulma.sass";
+</style>
