e35b0b2b3de69f5030201ac7638440af51eec63d
Shigeyuki-fukuda <fukuda@stores.jp>
2018-06-29 20:22:12 +0900

    Todoリストの叩き台作った

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'todoList',
+    data: function () {
+      return { todo: "" }
+    }
+  }
+</script>
+
+<style lang="scss">
+@import "../../node_modules/bulma/bulma.sass";
+</style>
